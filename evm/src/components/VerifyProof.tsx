41e585f4423d2bfec441903ba261d6b59eab624f
TheLedgerOfJoudi <hadi.mohammad.saleh1@gmail.com>
2024-02-10 22:34:13 +0300

    simplify wagmi

--- evm/src/components/VerifyProof.tsx
+++ evm/src/components/VerifyProof.tsx
@@ -1,108 +1,91 @@
-import { useState } from 'react'
+import { useState } from "react";
 import {
   useContractWrite,
   usePrepareContractWrite,
-  useWaitForTransaction
-} from 'wagmi'
-import { abi } from '../contracts-artifacts/abi'
-import {
-  Button,
-  Spinner,
-  useToast,
-  Text,
-  Flex,
-  Textarea
-} from '@chakra-ui/react'
-
+  useWaitForTransaction,
+} from "wagmi";
+import { abi } from "../contracts-artifacts/abi";
 
 export default function VerifyProof() {
-  const [dataStr, setDataStr] = useState('{}')
-  const [enabled, setEnabled] = useState(false)
-  const toast = useToast()
+  const [dataStr, setDataStr] = useState("{}");
+  const [enabled, setEnabled] = useState(false);
 
   const { config } = usePrepareContractWrite({
     enabled: enabled,
-    address: '0xacae5e972FBBEC5cc761a5F75705Ba041AC870Be',
+    address: "0xacae5e972FBBEC5cc761a5F75705Ba041AC870Be",
     abi: abi,
-    functionName: 'verifyProof',
+    functionName: "verifyProof",
     chainId: 420,
     args: [JSON.parse(dataStr)],
     onSuccess(data) {
-      console.log('Successfully Prepared', data)
+      console.log("Successfully Prepared", data);
     },
     onError(error) {
-      console.log('Error in Prepare')
-      console.log(error)
-      toast({
-        title: 'Please Enter a valid proof',
-        description: error.message,
-        duration: 5000,
-        isClosable: true,
-        position: 'top-right',
-        status: 'error'
-      })
-    }
-  })
+      console.log("Error in Prepare");
+      console.log(error);
+    },
+  });
   const { data, write, isLoading, isSuccess, isError, error } =
-    useContractWrite(config)
+    useContractWrite(config);
 
   const waitForTransaction = useWaitForTransaction({
     hash: data?.hash,
     onSettled(data, error) {
-      toast({
-        title: 'tx settled',
-        description: '',
-        duration: 5000,
-        isClosable: true,
-        position: 'top-right',
-        status: 'success'
-      })
-      console.log(data?.logs)
-    }
-  })
+      console.log(data?.logs);
+    },
+  });
 
   return (
-    <Flex
-      maxWidth={'container.lg'}
-      justifyContent={'center'}
-      justifyItems={'center'}
-      width={'100%'}
-    >
-      <Flex direction='column' gap='2' minWidth={'600px'}>
-        <Text>Please place your proof here!</Text>
-        <Textarea onChange={e => setDataStr(e.target.value)} />
-        <Button
-          disabled={!enabled}
-          colorScheme='blue'
-          onClick={() => {
-            setEnabled(!enabled)
-          }}
-        >
-          {!enabled ? 'Enable' : 'Disable'} Verification Before Sending tx
-        </Button>
-        <Button
-          disabled={!enabled}
-          colorScheme='blue'
-          onClick={() => {
-            if (!enabled) {
-              toast({
-                title: 'Please enable verify Button',
-                description: 'Please enter a valid proof',
-                duration: 5000,
-                isClosable: true,
-                position: 'top-right',
-                status: 'error'
-              })
-              return
-            }
-            write?.()
-          }}
-        >
-          Verify Proof{isLoading && <Spinner />}
-        </Button>
+    <div className="container">
+      <p>Please place your proof here!</p>
+      <textarea
+        className="area"
+        rows={25}
+        cols={50}
+        onChange={(e) => setDataStr(e.target.value)}
+      />
+      <button
+        className="button"
+        onClick={() => {
+          console.log(enabled);
+          setEnabled(!enabled);
+        }}
+      >
+        {!enabled ? "Enable" : "Disable"} Verification Before Sending tx
+      </button>
+      <button
+        className="button"
+        disabled={!enabled}
+        onClick={() => {
+          if (!enabled) {
+            return;
+          }
+          write?.();
+        }}
+      >
+        Verify Proof{isLoading && <p> Proving...</p>}
+      </button>
 
-        {isError && <Text>{error?.message ?? 'Error'}</Text>}
-      </Flex>
-    </Flex>
-  )
+      {isError && <p>{error?.message ?? "Error"}</p>}
+      <style jsx>{`
+        .container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+        }
+        .area {
+          padding: 10px;
+          border: solid 1px #ccc;
+          margin: 0 0 20px;
+          border-radius: 3px;
+        }
+        .button {
+          border: solid 1px #ccc;
+          margin: 0 0 20px;
+          border-radius: 3px;
+        }
+      `}</style>
+    </div>
+  );
 }
